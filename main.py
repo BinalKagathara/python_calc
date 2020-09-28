a7238e8cf12a4b3765c038380b12dbd2c1780259
binallkagathara <binalkagathara@gmail.com>
2020-09-28 13:53:17 +0530

    Created :model.py is created and main.py is updated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from view import GUI
+from  model import evaluateExpression
+
 
 # Client code
 def main():
@@ -11,6 +13,9 @@ def main():
     view = GUI()
     view.show()
     sys.exit(pycalc.exec_())
+
+
+    model = evaluateExpression
 
 if __name__ == "__main__":
     main()
