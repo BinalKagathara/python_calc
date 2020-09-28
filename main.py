e90acfad7bd0dcc9ec9470225a23f6d607d46289
binallkagathara <binalkagathara@gmail.com>
2020-09-28 16:19:16 +0530

    updated main.py file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from view import GUI
+from controller import Controller
 from  model import evaluateExpression
 
 
@@ -12,10 +13,14 @@ def main():
     # Show the calculator's GUI
     view = GUI()
     view.show()
+
+    model = evaluateExpression
+    Controller(model=model, view=view)
+
     sys.exit(pycalc.exec_())
 
 
-    model = evaluateExpression
+    
 
 if __name__ == "__main__":
     main()
